92db0fb4d5fbdcb995433474ea3b52724ab1c0aa
rishabh gehlot <rishabh3x@gmail.com>
2024-02-16 19:57:49 +0530

    last ai commit

--- src/pages/AiPage.jsx
+++ src/pages/AiPage.jsx
@@ -83,7 +83,7 @@ const ChatApp = () => {
 
 
         try {
-            const response = await fetch('http://localhost:5000', {
+            const response = await fetch('https://server-gimini.vercel.app/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
